88ccddc96c1b3710d557c12bd6952e3e4b19b1ee
Matias Arias <princable973@gmail.com>
2022-07-17 22:33:00 -0400

    Revert "informatica"
    This reverts commit fe905b45934a49571e605201aa5b6f0ad0da73c5.

--- Assets/Scripts/Informatica/Panel.cs
+++ Assets/Scripts/Informatica/Panel.cs
@@ -5,23 +5,71 @@ using UnityEngine.UI;
 
 public class Panel : MonoBehaviour
 {
-   //hacemos una lista de objetos
-    public GameObject[] Armado;
-    public GameObject Final;
+   // public GameObject piezas;
+    public Text texto_panel;
 
-    //si todos los objetos estan armados, se activa el objeto final
-    public void Activar()
+    public List<GameObject> armado = new List<GameObject>();
+    public List<GameObject> piezas = new List<GameObject>();
+
+    static int conteo;
+    public GameObject final;
+
+    public GameObject detectores;
+
+    public TextosUNABOT textos;
+
+    void Start()
     {
-        foreach (GameObject obj in Armado)
+        for (int n = 0; n < 7; n++)
         {
-            if (obj.activeSelf == false)
-            {
-                return;
-            }
+            armado[n].SetActive(true);
+            piezas[n].SetActive(false);
         }
-        Final.SetActive(true);
+        final.SetActive(false);
+        detectores.SetActive(false);
     }
 
 
-    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //piezas.SetActive(true);
+        //for (int n = 0; n < 7; n++)
+        //{
+        //    armado[n].SetActive(false);
+        //    piezas[n].SetActive(true);
+        //}
+        //detectores.SetActive(true);
+        //texto_panel.text = "...Armado en proceso...";
+        //conteo = 0;
+        //textos.var_siguiente = 1;
+        //textos.Burbujas();
+    }
+
+    public void Final()
+    {
+        conteo = conteo + 1;
+        Debug.Log(conteo);
+        if (conteo == 7)
+        {
+            final.SetActive(true);
+            texto_panel.text = "Presional para Salir";
+        }
+        
+    }
+
+    public void BotonInicio()
+    {
+        //piezas.SetActive(true);
+        for (int n = 0; n < 7; n++)
+        {
+            armado[n].SetActive(false);
+            piezas[n].SetActive(true);
+        }
+        detectores.SetActive(true);
+        texto_panel.text = "...Armado en proceso...";
+        conteo = 0;
+        textos.var_siguiente = 1;
+        textos.Burbujas();
+    }
 }
