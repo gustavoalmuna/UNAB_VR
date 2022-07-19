1381fe557e3a8d28190ada56d713b960b79ab7f7
Aaron del Risco <jdelriscodiaz@gmail.com>
2022-07-18 22:02:37 -0500

    Update HeartScript.cs

--- Assets/Scenes/Medicina/HeartScript.cs
+++ Assets/Scenes/Medicina/HeartScript.cs
@@ -3,18 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HeartScript : MonoBehaviour
-{ OVRGrabbable ovrGrababble;
-    
-  
-    // Start is called before the first frame update
-    void Start()
-    {
-        ovrGrababble = GetComponent<OVRGrabbable>();
-    }
+{ 
+  public  void palpito() {
 
-    // Update is called once per frame
-    void Update()
-    {
-        KeepVibing.KVInstance.TriggerVibration(4,2,5,ovrGrababble.grabbedBy.GetController); 
+
+        OVRInput.SetControllerVibration(2, 9);
+
     }
 }
