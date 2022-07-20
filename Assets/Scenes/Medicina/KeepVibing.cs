bcbf0717dba8a228354935f4c291d9ea1c9efbb4
Aaron del Risco <jdelriscodiaz@gmail.com>
2022-07-20 14:20:55 -0500

    Cambios a la escena de medicina.
    Añadí una pequeña animación al corazón roto, aparte de un trigger para que apenas se ponga el corazón, desaparezca y se active la animación

--- Assets/Scenes/Medicina/KeepVibing.cs
+++ Assets/Scenes/Medicina/KeepVibing.cs
@@ -1,58 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class KeepVibing : MonoBehaviour
 {
-   public static KeepVibing KVInstance;
+    Animator a;
 
-    private void Awake()
+
+    private void Start()
     {
-        KVInstance = this;
-    }
-  public  void TriggerVibration(AudioClip a,OVRInput.Controller c) {
+        a = GameObject.Find("EscenaMedicina").GetComponent<Animator>();
 
-
-
-        OVRHapticsClip vibing = new OVRHapticsClip(a);
-
-
-        if (c == OVRInput.Controller.LTouch) {
-
-            OVRHaptics.LeftChannel.Preempt(vibing); 
-        
-        }
-        if (c == OVRInput.Controller.RTouch) {
-
-            OVRHaptics.RightChannel.Preempt(vibing); 
-        
-        }
-
-    }
-       public void TriggerVibration(int iteration, int frequency, int fuerza, OVRInput.Controller c) {
-
-
-
-        OVRHapticsClip vibing = new OVRHapticsClip();
-
-        for (int i = 0;i<iteration; i++) {
-
-            vibing.WriteSample(i % frequency == 0 ? (byte)fuerza : (byte)0);
-        
-        }
-
-
-        if (c == OVRInput.Controller.LTouch) {
-
-            OVRHaptics.LeftChannel.Preempt(vibing); 
-        
-        }
-        if (c == OVRInput.Controller.RTouch) {
-
-            OVRHaptics.RightChannel.Preempt(vibing); 
-        
-        }
 
     }
 
+
+    public void AnimacionTrue() {
+
+        a.SetTrigger("DesencadenarAnimacion");
+
+    }
 }
+
