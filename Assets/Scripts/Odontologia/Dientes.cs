36d4e0fc032fa479706e6517e8770110a560b7a1
Matias Arias <princable973@gmail.com>
2022-07-25 08:27:02 -0400

    Derecho

--- Assets/Scripts/Odontologia/Dientes.cs
+++ Assets/Scripts/Odontologia/Dientes.cs
@@ -7,10 +7,31 @@ public class Dientes : MonoBehaviour
     public GameObject[] dientesBuenos;
     public GameObject[] dientesMalos;
 
-    //hacemos una funcion para que active dientes malos random cada determinado tiempo
-    public void activarDientesMalos()
+
+    void Start()
     {
-        int random = Random.Range(0, dientesMalos.Length);
-        dientesMalos[random].SetActive(true);
+        dientesMalos[0].SetActive(true);
+        dientesBuenos[0].SetActive(false);
+
+        //si objeto con tag "herramienta" entra en el trigger de diente malo se activa el diente bueno y se desactiva el diente malo
+       
+
+        
+        
     }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Herramienta")
+        {
+            dientesMalos[0].SetActive(false);
+            dientesBuenos[0].SetActive(true);         
+        }
+    }
+
+    
+    
+
+    
+
 }
