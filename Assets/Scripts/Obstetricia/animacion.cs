19007182d1ec524ed7d2aeb1f2f29ba4150d274b
Matias Arias <princable973@gmail.com>
2022-07-28 00:17:27 -0400

    update

--- Assets/Scripts/Obstetricia/animacion.cs
+++ Assets/Scripts/Obstetricia/animacion.cs
@@ -10,6 +10,9 @@ public class animacion : MonoBehaviour
     public GameObject obj2;
     public GameObject obj3;
     public GameObject obj4;
+    public GameObject obj5;
+    public GameObject obj6;
+    public GameObject obj7;
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +21,22 @@ public class animacion : MonoBehaviour
         anim.SetBool("Activo", true);
         obj3.SetActive(true);
         obj4.SetActive(true);
-        Invoke("activar", 1.5f);
+        Invoke("activar", 2f);
+        obj5.SetActive(true);
+        Invoke("activar2", 3f);
     }
 
     void activar()
     {
         obj1.SetActive(true);
-        obj2.SetActive(true);        
+        obj2.SetActive(true);
+        obj6.SetActive(true);
+                
+    }
+
+    void activar2()
+    {
+        obj7.SetActive(true);
     }
 
     
