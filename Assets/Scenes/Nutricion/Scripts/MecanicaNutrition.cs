b6b639e7902f854036a02b644d2d07c689717d3b
Aaron del Risco <jdelriscodiaz@gmail.com>
2022-08-09 21:40:13 -0500

    mecanicaNutricion

---
+++ Assets/Scenes/Nutricion/Scripts/MecanicaNutrition.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MecanicaNutrition : MonoBehaviour
+{
+    int casos=1;
+    public GameObject Pizza, Pastas,Pure, ArrozCCarne;
+    Vector3 firstPosP, firstPosPas, firstPosPur, firstPosAcc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstPosAcc = ArrozCCarne.transform.position;
+        firstPosP = Pizza.transform.position;
+        firstPosPas = Pastas.transform.position;
+        firstPosPur = Pure.transform.position;
+       
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name.Equals("grabPlateCarne") && casos == 1)
+        {
+
+            casos = 2;
+            ArrozCCarne.transform.position = firstPosAcc;
+
+        }
+        else if (other.gameObject.name.Equals("grabPlatePizza")&& casos == 1) {
+
+            Pizza.transform.position = firstPosP;
+        } 
+        
+        else if (other.gameObject.name.Equals("grabPlateSpageti")&& casos == 1) {
+
+            Pastas.transform.position = firstPosPas;
+        }  
+        else if (other.gameObject.name.Equals("grabPlatePure")&& casos == 1) {
+
+            Pure.transform.position = firstPosPur;
+        }
+        //Caso 2
+        if (other.gameObject.name.Equals("grabPlatePure") && casos == 2)
+        {
+
+            casos = 2;
+            Pure.transform.position = firstPosPur;
+
+        }
+        else if (other.gameObject.name.Equals("grabPlatePizza")&& casos == 2) {
+            
+            Pizza.transform.position = firstPosP;
+        } 
+        
+        else if (other.gameObject.name.Equals("grabPlateSpageti")&& casos == 2) {
+
+            Pastas.transform.position = firstPosPas;
+        }  
+        else if (other.gameObject.name.Equals("grabPlateCarne")&& casos == 2) {
+
+            ArrozCCarne.transform.position = firstPosAcc;
+        }
+    }
+}
