3747530728f783204d20c94d95ff15bee8e58875
Matias Arias <princable973@gmail.com>
2022-09-05 01:15:10 -0300

    Update

--- Assets/basuraBotada.cs
+++ Assets/basuraBotada.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class basuraBotada : MonoBehaviour
 {
+    public GameObject[] plantas;
     public BarraVida barraVida;
 
     //al entrar la basura en el collider 
     private void OnTriggerEnter(Collider other)
     {
         barraVida.vidaActual += 9;
-        
+        //activamos una planta aleatoria
+        plantas[Random.Range(0, plantas.Length)].SetActive(true);
     }
+    
+
+
 }
